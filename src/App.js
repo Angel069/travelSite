fed8fe8017e1a37d1bfc3782ef39655b5f47404b
Angel Brand <angel.brand062@gmail.com>
2023-06-15 12:18:54 -0600

    actualice la ruta de app.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './app.css'
+import '/travel/src/app.css'
 import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Main from "./Components/Main/Main";
